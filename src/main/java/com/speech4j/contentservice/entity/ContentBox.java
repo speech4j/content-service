6d4729072817c550b5191717c9593c1455c14ee1
MarrMika <slobodjanmarija1@gmail.com>
2020-04-03 15:01:37 +0300

    Connect to db and add repository

--- src/main/java/com/speech4j/contentservice/entity/ContentBox.java
+++ src/main/java/com/speech4j/contentservice/entity/ContentBox.java
@@ -5,7 +5,13 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.List;
 
 @Getter
@@ -13,8 +19,14 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(name = "contents")
 public class ContentBox {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String contentGuid;
+    @ElementCollection
     private List<String> tags;
     private String contentUrl;
     private String transcript;
--- src/main/java/com/speech4j/contentservice/repository/ContentBoxRepository.java
+++ src/main/java/com/speech4j/contentservice/repository/ContentBoxRepository.java
@@ -1,7 +1,9 @@
 package com.speech4j.contentservice.repository;
 
+import com.speech4j.contentservice.entity.ContentBox;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ContentBoxRepository{
+public interface ContentBoxRepository extends CrudRepository<ContentBox, String> {
 }
