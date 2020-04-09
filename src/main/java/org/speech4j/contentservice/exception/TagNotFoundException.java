ec845f4eb1cd10ccfa908b153c401f053bc08ad6
MarrMika <slobodjanmarija1@gmail.com>
2020-04-09 13:21:44 +0300

    Delete tagnotfound exception

--- src/main/java/org/speech4j/contentservice/exception/TagNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.speech4j.contentservice.exception;
-
-public class TagNotFoundException extends EntityNotFoundException {
-    public TagNotFoundException(String message) {
-        super(message);
-    }
-}
