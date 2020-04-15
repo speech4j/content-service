3c9131fcb8c0f88b83d5d78063aadf57cfeb28fa
MarrMika <slobodjanmarija1@gmail.com>
2020-04-15 16:49:07 +0300

    Implement datasource config

---
+++ src/main/java/org/speech4j/contentservice/config/DataSourceConfig.java
@@ -0,0 +1,35 @@
+package org.speech4j.contentservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Value("${db.driver}")
+    private String driverClassName;
+
+    @Value("${db.url}")
+    private String url;
+
+    @Value("${db.username}")
+    private String username;
+
+    @Value("${db.password}")
+    private String password;
+
+    @Bean
+    public DataSource getDatasource() {
+        return DataSourceBuilder.create()
+                .driverClassName(driverClassName)
+                .url(url)
+                .username(username)
+                .password(password)
+                .build();
+    }
+}
