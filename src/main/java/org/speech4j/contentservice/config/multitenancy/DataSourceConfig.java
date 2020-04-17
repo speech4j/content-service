fb2ecae2222fcf1a60c57a024cad78ddfade727e
MarrMika <slobodjanmarija1@gmail.com>
2020-04-17 11:26:37 +0300

    Add connection provider class

--- src/main/java/org/speech4j/contentservice/config/multitenancy/DataSourceConfig.java
+++ src/main/java/org/speech4j/contentservice/config/multitenancy/DataSourceConfig.java
@@ -1,4 +1,4 @@
-package org.speech4j.contentservice.config;
+package org.speech4j.contentservice.config.multitenancy;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
