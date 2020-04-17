0ad692614032d2498524d264a8293c110ee758c5
MarrMika <slobodjanmarija1@gmail.com>
2020-04-17 11:56:46 +0300

    Register request interceptor. Fix the getting of a tenant id from request.

---
+++ src/main/java/org/speech4j/contentservice/config/multitenancy/InterceptorConfig.java
@@ -0,0 +1,19 @@
+package org.speech4j.contentservice.config.multitenancy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class InterceptorConfig extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    HandlerInterceptor tenantInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(tenantInterceptor);
+    }
+}
--- src/main/java/org/speech4j/contentservice/config/multitenancy/TenantInterceptor.java
+++ src/main/java/org/speech4j/contentservice/config/multitenancy/TenantInterceptor.java
@@ -1,11 +1,13 @@
 package org.speech4j.contentservice.config.multitenancy;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 @Component
 public class TenantInterceptor extends HandlerInterceptorAdapter {
@@ -13,7 +15,8 @@ public class TenantInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
-        String tenantId = request.getParameter("tenantId");
+        Map pathVariables = (Map) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        String tenantId = (String) pathVariables.get("tenantId");
         TenantContext.setCurrentTenant(tenantId);
 
         return true;
